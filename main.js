f563970aeccfbbbace85f4b390da94ae67720e70
gitname <82920762+dsa02070@users.noreply.github.com>
2022-03-10 21:37:18 +0900

    0310 저장 장소 마련
    0310 저장 장소 마련

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ const $bonus = document.querySelector('#bonus');
 const bodyElem = document.querySelector('body');
 const startBtn = document.getElementById('start');
 const reloadBtn = document.getElementById('reload');
-const saveBtn = document.getElementById('save')
+// const saveBtn = document.getElementById('save')
 
 const showBall = (number, $target) => {
   const $ball = document.createElement('div');
@@ -86,44 +86,75 @@ reloadBtn.addEventListener('click', function () {
     childNodeDelete()
     location.reload();
   }
+  sessionStorage.clear()
+})
+// 2022 03 10
+
+let storeInput01 = document.getElementById("store_input01");
+let storeInput02 = document.getElementById("store_input02");
+let storeInput03 = document.getElementById("store_input03");
+let storeInput04 = document.getElementById("store_input04");
+let storeInput05 = document.getElementById("store_input05");
+
+let siStoreBtn01 = document.getElementById("si_store01");
+let siStoreBtn02 = document.getElementById("si_store02");
+let siStoreBtn03 = document.getElementById("si_store03");
+let siStoreBtn04 = document.getElementById("si_store04");
+let siStoreBtn05 = document.getElementById("si_store05");
+
+let siLoadBtn01 = document.getElementById('si_load01');
+let siLoadBtn02 = document.getElementById('si_load02');
+let siLoadBtn03 = document.getElementById('si_load03');
+let siLoadBtn04 = document.getElementById('si_load04');
+let siLoadBtn05 = document.getElementById('si_load05');
+
+let a = winballs.join()
+let b = winballs.join(' ')
+
+let storeInputValue = storeInput01.value;
+
+// 저장하기 버튼 클릭
+siStoreBtn01.addEventListener('click', function () {
+  storeInput01.setAttribute('value', b + ' ' + bonus)
+  sessionStorage.setItem('01', storeInput01.value)
+})
+siStoreBtn02.addEventListener('click', function () {
+  storeInput02.setAttribute('value', b + ' ' + bonus)
+  sessionStorage.setItem('02', storeInput02.value)
+})
+siStoreBtn03.addEventListener('click', function () {
+  storeInput03.setAttribute('value', b + ' ' + bonus)
+  sessionStorage.setItem('03', storeInput03.value)
+})
+siStoreBtn04.addEventListener('click', function () {
+  storeInput04.setAttribute('value', b + ' ' + bonus)
+  sessionStorage.setItem('04', storeInput04.value)
+})
+siStoreBtn05.addEventListener('click', function () {
+  storeInput05.setAttribute('value', b + ' ' + bonus)
+  sessionStorage.setItem('05', storeInput05.value)
 })
 
-
-// 저장 버튼
-saveBtn.addEventListener('click', function () {
-  // if ($bonus.childElementCount >= 1) {
-  //   for (let i = 0; i < 6; i++) {
-  //     showBall(winballs[i], history)
-  //   }
-  // }
-  if ($bonus.childElementCount >= 1) {
-    sessionStorage.setItem("key", JSON.stringify(winballs))
-    const winballsJson = JSON.stringify(winballs)
-    console.log(winballsJson);
-  } else {
-    alert('번호를 먼저 생성해주세요.')
-  }
+// 불러오기 버튼 클릭
+siLoadBtn01.addEventListener('click', function () {
+  let se01 = sessionStorage.getItem('01');
+  storeInput01.setAttribute('value', se01);
+})
+siLoadBtn02.addEventListener('click', function () {
+  let se02 = sessionStorage.getItem('02');
+  storeInput02.setAttribute('value', se02);
+})
+siLoadBtn03.addEventListener('click', function () {
+  let se03 = sessionStorage.getItem('03');
+  storeInput03.setAttribute('value', se03);
+})
+siLoadBtn04.addEventListener('click', function () {
+  let se04 = sessionStorage.getItem('04');
+  storeInput04.setAttribute('value', se04);
+})
+siLoadBtn05.addEventListener('click', function () {
+  let se05 = sessionStorage.getItem('05');
+  storeInput05.setAttribute('value', se05);
 })
 
-let history = document.querySelector('.history li')
-let printBtn = document.getElementById('print');
-// 출력 버튼
-printBtn.addEventListener('click', function () {
-
-  var output = sessionStorage.getItem("key");
-  var arr = JSON.parse(output);
-  console.log(arr)
-
-  for (let i = 0; i < 6; i++) {
-    showBall(arr[i], history)
-  }
-  const $ball2 = document.createElement('br');
-  history.append($ball2)
-})
-
-function jsonNumber() {
-  fetch("./number.json")
-    .then((response) => response.json())
-    .then((json) => console.log(json[0].result));
-}
-jsonNumber()
+// 2022 03 10 end
